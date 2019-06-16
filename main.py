30a5c0f58540815ece02900a913e6cd9d1f68a5f
Tyler Westland <tylerofthewest@gmail.com>
2019-06-16 14:05:46 -0400

    Redoes things to use bot commands.
    Huge change of almost everything
    to use bot commands.

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ Main for Running the Discord Bot
 :copyright: (c) 2019 i-question-this
 :license: GPL-3.0, see LICENSE for more details.
 """
-
+from discordplayspyboy import version_info
 from discordplayspyboy.config import TOKEN
-from discordplayspyboy.discordBot import Client
+from discordplayspyboy.discordBot import Bot
 
-# Set up the client
-client = Client()
+# Set up the bot
+description = "Interface to a GameBoy emulator."
+description += "\n Version: {}.{}.{}".format(version_info.major, version_info.minor, version_info.micro)
+bot = Bot('.', description=description, case_insensitive=True)
 
-# Run the client
-client.run(TOKEN)
+# Run the bot
+bot.run(TOKEN)
+
